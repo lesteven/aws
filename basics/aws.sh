ae9cbefc004fa1c52de245e7547d33e4d2ece132
lesteven <le_steven@outlook.com>
2019-04-06 15:41:57 -0700

    chore: add config comment

--- basics/aws.sh
+++ basics/aws.sh
@@ -1,6 +1,8 @@
 #!
 source ./aws.config
 
+# values will be written to ~/.aws/credentials
+
 aws configure set aws_access_key_id $keyid
 aws configure set aws_secret_access_key $accesskey
 aws configure set default.region $region
