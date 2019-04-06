8acb01086bdcb3a41d279f235f64ea40cae34fb1
lesteven <le_steven@outlook.com>
2019-04-06 15:57:18 -0700

    chore: update var name

--- basics/aws.sh
+++ basics/aws.sh
@@ -1,9 +1,9 @@
-#!
+#!/bin/bash
 source ./aws.config
 
 # values will be written to ~/.aws/credentials
 
 aws configure set aws_access_key_id $keyid
-aws configure set aws_secret_access_key $accesskey
+aws configure set aws_secret_access_key $secretkey
 aws configure set default.region $region
 aws iam get-user
